cbc4b1bd431cb2fda8831d27bb108fe7b65bf493
ArtemProkopev <artemprokopev@internet.ru>
2025-09-18 00:40:54 +0300

    chore: remove build artifacts and secrets

--- apps/api/ormconfig.ts
+++ apps/api/ormconfig.ts
@@ -1,16 +1,16 @@
-import { DataSource } from 'typeorm';
-import { User } from './src/users/user.entity';
-import { RefreshToken } from './src/auth/refresh-token.entity';
-import 'dotenv/config';
+import 'dotenv/config'
+import { DataSource } from 'typeorm'
+import { RefreshToken } from './src/auth/refresh-token.entity'
+import { User } from './src/users/user.entity'
 
 export default new DataSource({
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT || 5432),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  entities: [User, RefreshToken],
-  synchronize: false,
-  migrations: ['apps/api/migrations/*.ts'],
-});
+	type: 'postgres',
+	host: process.env.POSTGRES_HOST,
+	port: Number(process.env.POSTGRES_PORT || 5432),
+	username: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB,
+	entities: [User, RefreshToken],
+	synchronize: false,
+	migrations: ['apps/api/migrations/*.ts'],
+}) 
--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    super({
-      jwtFromRequest: ExtractJwt.fromExtractors([(req: any) => req?.cookies?.access_token || null]),
-      secretOrKey: process.env.JWT_ACCESS_SECRET,
-    });
-  }
-  async validate(payload: { sub: string; email: string }) {
-    return payload;
-  }
+	constructor() {
+		super({
+			jwtFromRequest: ExtractJwt.fromExtractors([
+				(req: any) => req?.cookies?.access_token || null,
+			]),
+			secretOrKey: process.env.JWT_ACCESS_SECRET,
+		})
+	}
+	async validate(payload: { sub: string; email: string }) {
+		return payload
+	}
 }
