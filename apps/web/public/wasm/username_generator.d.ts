29005a99b08c7be302547ba808fc74822a7b78ae
ArtemProkopev <artemprokopev@internet.ru>
2026-02-14 15:20:00 +0300

    chore(web): коммичу WASM-артефакты, чтобы проект собирался без локальной сборки WASM

---
+++ apps/web/public/wasm/username_generator.d.ts
@@ -0,0 +1,47 @@
+/* tslint:disable */
+/* eslint-disable */
+export function generate_username(): string;
+export class UsernameGenerator {
+  free(): void;
+  [Symbol.dispose](): void;
+  generate_multiple(count: number): Array<any>;
+  generate_username(): string;
+  constructor();
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_usernamegenerator_free: (a: number, b: number) => void;
+  readonly generate_username: () => [number, number];
+  readonly usernamegenerator_generate_multiple: (a: number, b: number) => any;
+  readonly usernamegenerator_generate_username: (a: number) => [number, number];
+  readonly usernamegenerator_new: () => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_externrefs: WebAssembly.Table;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ apps/web/public/wasm/username_generator_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_usernamegenerator_free: (a: number, b: number) => void;
+export const generate_username: () => [number, number];
+export const usernamegenerator_generate_multiple: (a: number, b: number) => any;
+export const usernamegenerator_generate_username: (a: number) => [number, number];
+export const usernamegenerator_new: () => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
